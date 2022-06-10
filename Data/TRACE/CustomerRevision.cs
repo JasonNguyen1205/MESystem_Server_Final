b4f9f3101b2dac133fc7ba21aa0e9359bf5443ed
JasonNguyen1205 <thuong.nguyen.dev12@gmail.com>
2022-06-10 19:56:14 +0700

    printing

--- Data/TRACE/CustomerRevision.cs
+++ Data/TRACE/CustomerRevision.cs
@@ -2,18 +2,38 @@
 
 public class CustomerRevision
 {
-    public CustomerRevision(string? po, string? partNo, string? orderNo, string? rev, string? productFamily)
+    public CustomerRevision(string? po, 
+        string? partNo, 
+        string? orderNo, 
+        string? rev, 
+        string? productFamily
+
+        //string? lastestRev=null,
+        //string? activeDate = null,
+        //int status = 0,
+        //string? userConfirm = null
+        )
+       
     {
         PO = po;
         OrderNo = orderNo;
         PartNo = partNo;
         Rev = rev;
         ProductFamily = productFamily;
+
+        //LastestRev = lastestRev;
+        //ActiveDate = activeDate;
+        //Status = status;
+        //UserConfirm = userConfirm;
     }
 
-    public string PO { get; set; }
-    public string OrderNo { get; set; }
-    public string PartNo { get; set; }
-    public string Rev { get; set; }
-    public string ProductFamily { get; set; }
+    public string? PO { get; set; }
+    public string? OrderNo { get; set; }
+    public string? PartNo { get; set; }
+    public string? Rev { get; set; }
+    //public string? LastestRev { get; set; }
+    public string? ProductFamily { get; set; }
+    //public string? ActiveDate { get; set; }
+    //public int Status { get; set; }
+    //public string? UserConfirm { get; set; }
 }
