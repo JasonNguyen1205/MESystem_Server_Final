73bdeb35108c472353c79a7069c179b6741e5825
namvincent <namvincen@gmail.com>
2022-07-27 20:21:48 +0700

    add new Rework function
    add new Rework function

---
+++ Data/TRACE/Rework.cs
@@ -0,0 +1,51 @@
+﻿namespace MESystem.Data.TRACE
+{
+    public class Rework
+    {
+        public string? Rework_ID { get; set; }
+        public int? NG_Code { get; set; }
+        public string? NG_Description_Eng { get; set; }
+        public string? NG_Description_VN { get; set; }
+        public string? Barcode { get; set; }
+        public string? Customer_Barcode { get; set; }
+        public DateTime? Input_Date { get; set; }
+        public string? Contract { get; set; }
+        public string? Part_No { get; set; }
+        public string? Order_No { get; set; }
+        public string? User_Id { get; set; }
+        public string? Remark { get; set; }
+
+        public Rework(string? barcode, string? customer_Barcode, int? nG_Code, string? remark, string? part_No, string? order_No, string? user_Id)
+        {
+            Barcode = barcode;
+            Customer_Barcode = customer_Barcode;
+            NG_Code = nG_Code;
+            Remark = remark;
+            Part_No = part_No;
+            Order_No = order_No;
+            User_Id = user_Id;
+        }
+
+        public Rework(int? nG_Code, string? nG_Description_Eng, string? nG_Description_VN)
+        {
+            NG_Code = nG_Code;
+            NG_Description_Eng = nG_Description_Eng;
+            NG_Description_VN = nG_Description_VN;
+        }
+
+        public Rework()
+        {
+
+        }
+
+        public Rework(string? nG_Description_VN)
+        {
+            NG_Description_VN = nG_Description_VN;
+        }
+
+        public Rework(int? nG_Code)
+        {
+            NG_Code = nG_Code;
+        }
+    }
+}
---
+++ Pages/Process/ReworkPage.razor.cs
@@ -0,0 +1,86 @@
+﻿
+using MESystem.Service;
+using MESystem.Data.TRACE;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace MESystem.Pages.Process;
+
+public partial class ReworkPage : ComponentBase
+{
+    [Inject]
+    private TraceService? TraceDataService { get; set; }
+    public string? FocusElement { get; set; }
+
+    public string? ReadOnlyElement { get; set; }
+    bool ShowCheckboxes { get; set; } = true;
+    public IEnumerable<Rework>? Data { get; set; }
+    string? ng_Code { get; set; }
+    string? last_Code { get; set; }
+    string? first_Code { get; set; }
+    string? remark { get; set; }
+    public bool IsReady { get; set; }
+    public string? ng_data { get; set; }
+    public string? barcode { get; set; }
+    int flag { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        
+        Data = await TraceDataService.GetNGCode();
+
+        flag = 0;
+    }
+    async void GetData()
+    {
+
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+
+        }
+
+    }
+    private async void HandleCodeInput(KeyboardEventArgs e)
+    {
+        if (e.Key == "Enter")
+        {
+            if (flag == 0)
+            {
+                FocusElement = "ng_Code2";
+                ReadOnlyElement = "ng_Code1";
+                flag = 1;
+            }
+            else
+            {
+                FocusElement = "barcode";
+                ReadOnlyElement = "ng_Code2";
+                flag = 0;
+                ng_Code = first_Code + last_Code;
+                ng_data = Data.Where(e => e.NG_Description_VN.Split(".")[0].ToString() == ng_Code).FirstOrDefault()?.NG_Description_VN ?? "Wrong Rework Code!";
+            }
+        }
+
+    }
+
+    private async void HandleBarcodeInput(KeyboardEventArgs e)
+    {
+        if (e.Key == "Enter")
+        {
+            TraceDataService.GetBarcodeLink(barcode);
+            Rework input_data = new Rework(barcode,null,int.Parse(ng_Code),"","","","");
+            TraceDataService.InsertReworkData(input_data);
+        }
+    }
+
+    private async void HandleRemarkInput(KeyboardEventArgs e)
+    {
+        if (e.Key == "Enter")
+        {
+            FocusElement = "barcode";
+            ReadOnlyElement = "remark";
+        }
+    }
+}
